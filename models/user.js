ab8d2ee043c8e9e24f692eeb04cba9911d473714
theSamuelIyeh <105016275+theSamuelIyeh@users.noreply.github.com>
2023-04-20 22:08:36 +0100

    Update model

--- models/user.js
+++ models/user.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    gender: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true
-    }
-})
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+});
 
-module.exports = mongoose.Model(userSchema, 'User');
+module.exports = mongoose.model("User", userSchema);
+
+
